b2d52fbcfe433c0218f9dfa97c201f39d6f15581
Devansh <devgoe5115@gmail.com>
2024-10-13 20:36:44 +0530

    Deployment fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], resolve: {
+  plugins: [react()],
+  resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
+  },
+});
